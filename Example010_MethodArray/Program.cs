c617b2595b487beb3ffdd18445a374832460d26a
Vadim <jagabumba@gmail.com>
2023-03-04 03:20:26 +0700

    process

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] array = { 31, 22, 3, 411, 53, 64, 27, 811 };
+
+int n = array.Length;
+int find = 411;
+
+int index = 0;
+
+while (index < n)
+{
+  if (array[index] == find)
+  {
+    Console.WriteLine(index);
+  }
+  index++;
+}
