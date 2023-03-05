9c89b0e2c5f6ddfab1c1b013eeb188a6a33d83dc
Vadim <jagabumba@gmail.com>
2023-03-06 05:58:04 +0700

    Examples 010 011

--- Example010_MethodArray/Program.cs
+++ Example010_MethodArray/Program.cs
@@ -1,7 +1,7 @@
 ﻿int[] array = { 31, 22, 3, 411, 53, 64, 27, 811 };
 
 int n = array.Length;
-int find = 411;
+int find = 811;
 
 int index = 0;
 
@@ -10,6 +10,7 @@ while (index < n)
   if (array[index] == find)
   {
     Console.WriteLine(index);
+    break;
   }
   index++;
 }
---
+++ Example011_ArrayLibrary/Program.cs
@@ -0,0 +1,50 @@
+﻿void FillArray(int[] collection)
+{
+  int Length = collection.Length;
+  int index = 0;
+  while (index < Length)
+  {
+    collection[index] = new Random().Next(1, 10);
+    //index == index + 1;
+    index++;
+  }
+}
+
+void PrintArray(int[] col) //void - значит ничего не возращает
+{
+  int count = col.Length;
+  int position = 0;
+  while (position < count)
+  {
+    Console.WriteLine(col[position]);
+    position++;
+  }
+}
+
+int IndexOff(int[] collection, int find)
+{
+  int count = collection.Length;
+  int index = 0;
+  int position = -1;
+
+  while (index < count)
+  {
+    if (collection[index] == find)
+    {
+      position = index;
+      break;
+    }
+    index++;
+  }
+  return position;
+}
+
+int[] array = new int[10]; //new int - создай новый массив в котором будет 10 элементов
+
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+
+int pos = IndexOff(array, 4);
+Console.WriteLine(pos);
+
